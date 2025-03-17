7c1928e37c622595f4ee9175fc38b1e34604bc93
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-17 12:20:40 +0530

    first Push

---
+++ SeleniumProject/src/test/java/Pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void CreateOrgTest() {
+		System.out.println("Execute CreateOrgtest");
+	}
+	
+	@Test
+	public void ModifyOrgTest() {
+		System.out.println("Execute ModifyOrgTest");
+	}
+}
