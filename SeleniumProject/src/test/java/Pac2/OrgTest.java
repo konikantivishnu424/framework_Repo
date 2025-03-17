3fed9cd4c2109ba4e810237dd21b0739fba708ed
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-17 15:41:05 +0530

    orgTest new Method Added

--- SeleniumProject/src/test/java/Pac2/OrgTest.java
+++ SeleniumProject/src/test/java/Pac2/OrgTest.java
@@ -7,6 +7,16 @@ public class OrgTest {
 	@Test
 	public void CreateOrgTest() {
 		System.out.println("Execute CreateOrgtest");
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	
 	@Test
